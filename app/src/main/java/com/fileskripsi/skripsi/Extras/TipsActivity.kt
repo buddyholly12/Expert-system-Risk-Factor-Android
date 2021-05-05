4f8bfacce832f3da720543162feca6671c428294
buddyholly12 <tekweb2018@gmail.com>
2021-05-05 21:40:51 +0700

    Skripsi_file_Master_data

--- app/src/main/java/com/fileskripsi/skripsi/Extras/TipsActivity.kt
+++ app/src/main/java/com/fileskripsi/skripsi/Extras/TipsActivity.kt
@@ -8,5 +8,6 @@ class TipsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tips)
+        hhh
     }
 }
